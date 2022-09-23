7e2b2db2de437125676704d217c1fc23f3085967
Golda Gnanaselvi <golda@zaeplex.com>
2022-09-23 11:41:28 +0000

    Add project files.

---
+++ BlazorWebAssemblyApp/wwwroot/Scripts/SampleJavaScript.js
@@ -0,0 +1,31 @@
+﻿
+function Takeshot() {
+    let div =
+        document.getElementById('screenshotContainer');
+
+    html2canvas(div).then(
+        function (canvas) {
+            document
+                .getElementById('output')
+                .appendChild(canvas);
+        });
+}
+
+function DownloadImg() {
+    html2canvas(document.querySelector("#output")).then(canvas => {
+        saveAs(canvas.toDataURL(), 'screenshot.png');
+    });
+}
+
+function saveAs(uri, filename) {
+    var link = document.createElement('a');
+    if (typeof link.download === 'string') {
+        link.href = uri;
+        link.download = filename;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    } else {
+        window.open(uri);
+    }
+}
